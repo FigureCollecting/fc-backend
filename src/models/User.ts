050a058ac0600b78d0a0d98a9f467c095abc04af
Ross Goldberg <rpgoldberg@gmail.com>
2026-06-13 20:02:55 -0500

    Bump mongoose to 9.7

--- src/models/User.ts
+++ src/models/User.ts
@@ -92,14 +92,13 @@ const UserSchema = new Schema<IUser>(
 );
 
 // Hash password before saving
-UserSchema.pre('save', async function(next) {
+UserSchema.pre('save', async function() {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
-  
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Method to compare passwords
